f62f60055abeebed311d9481a7430357f17835be
Kowsar Islam <kowsarulislam@gmail.com>
2024-02-20 16:46:25 +0200

    Few changes made

---
+++ index.js
@@ -0,0 +1,27 @@
+//console.log("JS Loop and string self practice");
+
+//Just printing the loop number
+//for (let count = 1; count <= 15; count++) {
+    //console.log("Serial no");
+//}
+//console.log("Loop has ended");
+
+//Calculate sum of  1 to 10'
+
+//let totalSum = 0
+//for (let number = 0; number <= 5; number++) {
+    //totalSum = totalSum + number
+//}
+//console.log("Sum =", totalSum);
+
+//So for bigger no to count we can use new variavle inside the conditional staement
+
+//let sum = 0;
+//let newn = 50;
+//for (let number = 0; number <= newn; number++) {
+    //sum = sum + number
+    
+//}
+//console.log("sum =", sum);
+
+
